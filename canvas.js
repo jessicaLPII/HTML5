29240195677de0e01ee26ef530b4689179e31def
Jessica Mendes <jessicamendes.info@gmail.com>
2016-04-15 23:06:44 -0300

    canvas

--- canvas.js
+++ canvas.js
@@ -1,81 +1,3 @@
-/*function init(){
-	var canvas = document.getElementById('myCanvas');
-	canvas.addEventListener("click",desenha);
-}
-function desenha(event){
-	var canvas = document.getElementById('myCanvas');
-	var offsetY = canvas.offsetTop;
-	var offsetX = canvas.offsetLeft;
-	var ctx = canvas.getContext('2d');
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-
-	var x = event.clientX;
-	var y = event.clientY;
-	ctx.fillRect(x-offsetX,y-offsetY,150,150);
-	//alert("x="+x+",y="+y);
-}
-
-window.onload = init; código da aula passada
-*/
-/*function init(){
-	var canvas = document.getElementById('myCanvas');
-	var ctx = canvas.getContext('2d');
-	img.onload = function(){
-		ctx.save();// salvei um estado do contexto faço as alterações (proximas linhas)
-		ctx.scale(0.5,0.5); //reduz o conteudo do canvas em 50%
-		ctx.drawImage(img,0,0);//desenhar imagem no contexto do canvas (parametros: objeto e a posição)
-		// posso add a linha anterior novamente para duplicar a imagem, só mudar a posição da imagem
-		ctx.restore(); //restauro a imagem antes de 
-		var img = new Image(); // criar imagem (objeto do canvas para trabalhar com imagem)
-	}
-	
-	
-	
-	
-		
-	img.src="pinguins.jpg";
-}
-window.onload = init;*/
-
-
-
-/*function init(){
-	var canvas = document.getElementById('myCanvas');
-	var ctx = canvas.getContext('2d');
-	
-	var img = new Image();
-	img.onload = function(){
-		ctx.scale(0.5,0.5);		
-		ctx.drawImage(img,0,0);		
-		var imageData = ctx.getImageData(0,0,img.width,img.height);//
-		var dataArray = imageData.data;
-		for(var i=0;i<dataArray.length;i+=4){ // a cada interação ele anda 4 casas (vetor RGBalfa que ele passou no quadro)
-			var red = dataArray[i];
-			var green = dataArray[i+1];
-			var blue = dataArray[i+2];
-			var alpha = dataArray[i+3];
-			
-			/*dataArray[i] = 0.2*red;
-			dataArray[i+1] = 1*green;
-			dataArray[i+2] = 1*blue;
-			dataArray[i+3] = 1*alpha;
-			var bw = (red+green+blue)/3; // filtro preto e branco
-			
-			dataArray[i] = bw;
-			dataArray[i+1] = bw;
-			dataArray[i+2] = bw;
-			dataArray[i+3] = bw;
-			
-		}
-		ctx.putImageData(imageData,0,0);
-	}
-	img.src="pinguins.jpg";
-}
-window.onload = init;*/
-
-
-
-// fazer tres gatos, um com cada cor (RGB)
 function init(){
 	var canvas = document.getElementById('myCanvas');
 	var ctx = canvas.getContext('2d');	
@@ -209,80 +131,89 @@ function init(){
 		ctx.putImageData(imageData,0,0);
     	}
 
-    function carregaImagemBorder(){//img.onload = function(){
-		//ctx.scale(0.5,0.5);
-		ctx.drawImage(img,0,0);
-		//ctx.drawImage(img,img.width,0);
-		//ctx.drawImage(img,2*img.width,0);
+    function edge(input, context) {
+    var w = input.width, h = input.height;
+    var output = context.createImageData(w, h);
+    var inputData = input.data;
+    var outputData = output.data;
+    var bytesPerRow = w * 4;
+    var pixel = bytesPerRow + 4; // Start at (1,1)
+    var hm1 = h - 1;
+    var wm1 = w - 1;
+    for (var y = 1; y < hm1; ++y) {
+        // Prepare initial cached values for current row
 
-		var imageData = ctx.getImageData(0, 0, img.width, img.height);
-		var dataArray = imageData.data;
-		for(var i=0; i < dataArray.length; i+=4){
-			var red 	= dataArray[i];
-			var green	= dataArray[i+1];
-			var blue	= dataArray[i+2];
-			var alpha	= dataArray[i+3];
-			var bw = (red + green + blue)/3;
-			dataArray[i] = (1/4)*red + 0*red + (-1/4)*red;
-			dataArray[i+1] = (2/4)*green + 0*green + (-2/4)*green;
-			dataArray[i+2] = (1/4)*blue + 0*blue + (-1/4)*blue;
-			dataArray[i+3] = 1/4*alpha;
-		}
-		ctx.putImageData(imageData,0,0);
-    	}
+        var centerRow = pixel - 4;
+        var priorRow = centerRow - bytesPerRow;
+        var nextRow = centerRow + bytesPerRow;
 
-	/*img.onload = function(){
-		ctx.scale(0.5,0.5);
-		ctx.drawImage(img,0,0);
-		ctx.drawImage(img,img.width,0);
-		ctx.drawImage(img,2*img.width,0);
+        var r1 = inputData[priorRow]   + inputData[centerRow]   + inputData[nextRow];
+        var g1 = inputData[++priorRow] + inputData[++centerRow] + inputData[++nextRow];
+        var b1 = inputData[++priorRow] + inputData[++centerRow] + inputData[++nextRow];
 
-		var imageData = ctx.getImageData(0, 0, img.width*0.5, img.height*0.5);
-		var dataArray = imageData.data;
-		for(var i=0; i < dataArray.length; i+=4){
-			var red 	= dataArray[i];
-			var green	= dataArray[i+1];
-			var blue	= dataArray[i+2];
-			var alpha	= dataArray[i+3];
-			var bw = (red + green + blue)/3;
-			dataArray[i] = red;
-			dataArray[i+1] = 0;
-			dataArray[i+2] = 0;
-			dataArray[i+3] = 1*alpha;
-		}
-		ctx.putImageData(imageData,0,0);
+        var rp = inputData[priorRow += 2];
+        var gp = inputData[++priorRow];
+        var bp = inputData[++priorRow];
 
-		imageData = ctx.getImageData(img.width*0.5, 0, img.width*0.5, img.height*0.5);
-		dataArray = imageData.data;
-		for(var i=0; i < dataArray.length; i+=4){
-			var red 	= dataArray[i];
-			var green	= dataArray[i+1];
-			var blue	= dataArray[i+2];
-			var alpha	= dataArray[i+3];
-			var bw = (red + green + blue)/3;
-			dataArray[i] = 0;
-			dataArray[i+1] = green;
-			dataArray[i+2] = 0;
-			dataArray[i+3] = 1*alpha;
-		}
-		ctx.putImageData(imageData,img.width*0.5,0);
+        var rc = inputData[centerRow += 2];
+        var gc = inputData[++centerRow];
+        var bc = inputData[++centerRow];
 
+        var rn = inputData[nextRow += 2];
+        var gn = inputData[++nextRow];
+        var bn = inputData[++nextRow];
 
-		imageData = ctx.getImageData(img.width, 0, img.width*0.5, img.height*0.5);
-		dataArray = imageData.data;
-		for(var i=0; i < dataArray.length; i+=4){
-			var red 	= dataArray[i];
-			var green	= dataArray[i+1];
-			var blue	= dataArray[i+2];
-			var alpha	= dataArray[i+3];
+        var r2 = rp + rc + rn;
+        var g2 = gp + gc + gn;
+        var b2 = bp + bc + bn;
+        
+        // Main convolution loop
+        for (var x = 1; x < wm1; ++x) {
+            centerRow = pixel + 4;
+            priorRow = centerRow - bytesPerRow;
+            nextRow = centerRow + bytesPerRow;
 
-			dataArray[i] = 0;
-			dataArray[i+1] = 0;
-			dataArray[i+2] = blue;
-			dataArray[i+3] = 1*alpha;
-		}
-		ctx.putImageData(imageData,2*img.width*0.5,0);
-	}*/
+            var r = 0 + (rc << 3) - r1 - rp - rn;
+            var g = 0 + (gc << 3) - g1 - gp - gn;
+            var b = 0 + (bc << 3) - b1 - bp - bn;
+
+            r1 = r2;
+            g1 = g2;
+            b1 = b2;
+
+            rp = inputData[  priorRow];
+            gp = inputData[++priorRow];
+            bp = inputData[++priorRow];
+
+            rc = inputData[  centerRow];
+            gc = inputData[++centerRow];
+            bc = inputData[++centerRow];
+
+            rn = inputData[  nextRow];
+            gn = inputData[++nextRow];
+            bn = inputData[++nextRow];
+
+            r2 = rp + rc + rn;
+            g2 = gp + gc + gn;
+            b2 = bp + bc + bn;
+
+            outputData[pixel] = r - r2;
+            outputData[++pixel] = g - g2;
+            outputData[++pixel] = b - b2;
+            outputData[++pixel] = 255; // alpha
+            ++pixel;
+        }
+        pixel += 8;
+    }
+    return output;
+}
+
+function carregaImagemBorder() {
+      var imageData = ctx.getImageData(0, 0, img.width, img.height);
+      imageData = edge(imageData, ctx);
+
+      ctx.putImageData(imageData, 0, 0);
+}
 	img.src="cachorro.png";
 }
 window.onload = init;
